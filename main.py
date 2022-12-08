c04a7050798ae44f1681cb5341063053a4c6c8a9
Alberto Saenz <alb.saenz@gmail.com>
2022-12-07 22:07:20 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -7,23 +7,27 @@ import json
 app = Flask(__name__)
 api = Api(app)
 
-df = pd.DataFrame({'Autor': ['Miguel de Cervantes', 'Lope de Vega', 'Francisco de Quevedo'],
+Autor1 = 'Miguel de Cervantes'
+Autor2 = 'Lope de Vega'
+Autor3 = 'Francisco de Quevedo'
+
+df = pd.DataFrame({'Autor': [Autor1, Autor2, Autor3],
                    'Cita1': ['Oh, memoria, enemiga mortal de mi descanso', 'Los celos son hijos del amor, mas son bastardos, te confieso', 'El que quiere de esta vida todas las cosas a su gusto, tendrá muchos disgustos'],
                    'Cita2': ['La virtud mas es perseguida de los malos que amada de los buenos', 'Que de una mujer que es buena mil cosas buenas se aprenden', 'Las palabras son como monedas, que una vale por muchas como muchas no valen por una']})
 
 class Quotes(Resource):
     def get(self):
         return {
-            'Don Quijote de la Mancha': {
+            Autor1: {
                 'cita1': ['Oh, memoria, enemiga mortal de mi descanso'],
                 'cita2': ['La virtud mas es perseguida de los malos que amada de los buenos']
                 
         },
-        'Lope de Vega': {
+        Autor2: {
             'cita1': ['Los celos son hijos del amor, mas son bastardos, te confieso'],
             'cita2': ['Que de una mujer que es buena mil cosas buenas se aprenden']
             },
-        'Francisco de Quevedo': {
+        Autor3: {
             'cita1': ['El que quiere de esta vida todas las cosas a su gusto, tendrá muchos disgustos'],
             'cita2': ['Las palabras son como monedas, que una vale por muchas como muchas no valen por una']
             }
